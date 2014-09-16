50d7efa31d830ead2618b2afde2ab536fdad42c6
Konstantin Gründger <kammermark@gmx.de>
2014-09-16 08:27:10 +0200

    Switch to scene2d.ui

--- core/src/de/meisterschueler/gdx/screens/ChromaticContestScreen.java
+++ core/src/de/meisterschueler/gdx/screens/ChromaticContestScreen.java
@@ -1,12 +1,13 @@
 package de.meisterschueler.gdx.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 
@@ -15,11 +16,20 @@ import de.meisterschueler.gpgs.ScoreService;
 
 public class ChromaticContestScreen extends MidiScreen {
 
-	int lowest = 48;
+	private Stage stage;
+	private TextureAtlas atlas;
+	private Skin skin;
+	
+	private Label fpsLabel;
+	private Label fpsValue;
+	private Label timeLabel;
+	private Label timeValue;
+	private ProgressBar scoresProgress;
+	private Table table;
+	
+	static final int LOWEST_NOTE = 48;
+	static final int HIGHEST_NOTE = 55;
 	int current = 0;
-	int uppest = 55;
-
-	boolean keys[] = new boolean[8];
 
 	long startTime;
 	long deltaTime;
@@ -29,69 +39,68 @@ public class ChromaticContestScreen extends MidiScreen {
 
 	private ScoreService scoreService;
 
-	private ShapeRenderer shapeRenderer;
-	private SpriteBatch spriteBatch;
-	private BitmapFont font;
-
 	public ChromaticContestScreen(ScoreService scoreService) {
 		this.scoreService = scoreService;
+		
+		stage = new Stage();
 
-		shapeRenderer = new ShapeRenderer();
-		spriteBatch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.WHITE);
-
+		atlas = new TextureAtlas(Gdx.files.internal("uiskin.atlas"));
+		skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
+		
+		fpsLabel = new Label("FPS: ", skin);
+		fpsValue = new Label("", skin);
+		timeLabel = new Label("Time: ", skin);
+		timeValue = new Label("", skin);
+		scoresProgress = new ProgressBar(LOWEST_NOTE, HIGHEST_NOTE, 1, false, skin);
+		
+		table = new Table();
+		table.setFillParent(true);
+		table.add(fpsLabel).left();
+		table.add(fpsValue);
+		table.row();
+		table.add(timeLabel).left();
+		table.add(timeValue);
+		table.row();
+		table.add(scoresProgress);
+		
+		stage.addActor(table);
+		
 		init();
 	}
 
 	private void init() {
 		status = Status.READY;
-		current = lowest-1;
-		keys = new boolean[uppest-lowest+1];
+		current = LOWEST_NOTE-1;
 		deltaTime = 0;
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		
 		if (status == Status.RUNNING) {
 			deltaTime = System.currentTimeMillis()-startTime;
 		}
+		
+		fpsValue.setText(Integer.toString(Gdx.graphics.getFramesPerSecond()));
+		timeValue.setText(Double.toString(deltaTime/1000.0));
+		scoresProgress.setValue(current);
 
-		spriteBatch.begin();
-		font.draw(spriteBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 20, Gdx.graphics.getHeight()-20);
-		font.draw(spriteBatch, "Time: " + deltaTime/1000.0, 20, Gdx.graphics.getHeight()-40);
-		spriteBatch.end();
-
-		shapeRenderer.begin(ShapeType.Filled);
-		for (int i = 0; i < keys.length; i++) {
-			if (keys[i]) {
-				shapeRenderer.setColor(Color.GREEN);
-			} else {
-				shapeRenderer.setColor(Color.RED);
-			}
-			shapeRenderer.box(Gdx.graphics.getWidth()/keys.length*i, 0, 0, Gdx.graphics.getWidth()/keys.length, 20, 0);
-		}
-		shapeRenderer.end();
-
-		spriteBatch.begin();
-		font.draw(spriteBatch, "ChromaticEffect", 20, 20);
-		spriteBatch.end();
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
 	public void onMidiNoteOn(NoteOn noteOn) {
 		if (noteOn.getNote() == current + 1) {
 			current++;
-			keys[current-lowest]=true;
 		}
 
-		if (current == lowest) {
+		if (current == LOWEST_NOTE) {
 			status = Status.RUNNING;
 			startTime = System.currentTimeMillis();
-		} else if (current == uppest) {
+		} else if (current == HIGHEST_NOTE) {
 			status = Status.FINISHED;
 			long deltaTime = System.currentTimeMillis()-startTime;
 
