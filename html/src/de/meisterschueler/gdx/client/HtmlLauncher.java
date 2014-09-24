ed09869ad84c91c1159747cd4f2f6b51643ae04e
Konstantin Gründger <kammermark@gmx.de>
2014-09-24 22:12:38 +0200

    Add html support

--- html/src/de/meisterschueler/gdx/client/HtmlLauncher.java
+++ html/src/de/meisterschueler/gdx/client/HtmlLauncher.java
@@ -3,7 +3,11 @@ package de.meisterschueler.gdx.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import de.meisterschueler.gdx.MyGdxGame;
+
+import de.meisterschueler.basic.NoteOff;
+import de.meisterschueler.basic.NoteOn;
+import de.meisterschueler.gdx.Meisterschueler;
+import de.meisterschueler.gdx.MidiOutput;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +18,19 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new MyGdxGame();
+                return new Meisterschueler(new MidiOutput() {
+					
+					@Override
+					public void sendNoteOn(NoteOn noteOn) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void sendNoteOff(NoteOff noteOff) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
         }
 }
