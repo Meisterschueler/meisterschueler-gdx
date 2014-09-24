754fa99ee01539cf8482414c8ebcbdfbf219161a
Konstantin Gründger <kammermark@gmx.de>
2014-09-24 21:55:23 +0200

    Add IOS support

--- ios/src/de/meisterschueler/gdx/IOSLauncher.java
+++ ios/src/de/meisterschueler/gdx/IOSLauncher.java
@@ -5,13 +5,34 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import de.meisterschueler.gdx.MyGdxGame;
+
+import de.meisterschueler.basic.NoteOff;
+import de.meisterschueler.basic.NoteOn;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new MyGdxGame(), config);
+        Meisterschueler meisterschueler = new Meisterschueler(new MidiOutput() {
+			
+			@Override
+			public void sendNoteOn(NoteOn noteOn) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void sendNoteOff(NoteOff noteOff) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        meisterschueler.setScoreService(new ScoreServiceIOS(){
+        	
+        });
+        
+        return new IOSApplication(meisterschueler, config);
     }
 
     public static void main(String[] argv) {
---
+++ ios/src/de/meisterschueler/gdx/ScoreServiceIOS.java
@@ -0,0 +1,67 @@
+package de.meisterschueler.gdx;
+
+import de.meisterschueler.gpgs.ScoreService;
+
+public class ScoreServiceIOS implements ScoreService {
+
+	@Override
+	public boolean getSignedInGPGS() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void loginGPGS() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void submitEventGPGS(String id, int score) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void submitScoreGPGS(String id, int score) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unlockAchievementGPGS(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getLeaderboardGPGS(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getAchievementsGPGS() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void submitEventGPGS_chromaticStart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void submitScoreGPGS_chromatic(int score) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getLeaderboardGPGS_chromatic() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
