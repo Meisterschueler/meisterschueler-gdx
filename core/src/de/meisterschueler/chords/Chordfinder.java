85efb13ec5c7a9a4aa17175c4bbb4fb416d5f376
kammermark@gmx.de <kammermark@gmx.de>
2014-10-01 14:04:25 +0200

    Got chords from www.pluck-n-play.com

--- core/src/de/meisterschueler/chords/Chordfinder.java
+++ core/src/de/meisterschueler/chords/Chordfinder.java
@@ -1,10 +1,20 @@
-package de.meisterschueler.chords;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Chordfinder {
+
+	static private class Interval {
+
+		public Interval(String name, int semitones) {
+			super();
+			this.name = name;
+			this.semitones = semitones;
+		}
+
+		String name;
+		int semitones;
+	}
 
 	static private class Chord {
 
@@ -23,50 +33,139 @@ public class Chordfinder {
 		int steps[];
 	}
 
+	private static ArrayList<Interval> intervals = new ArrayList<Interval>();
 	private static ArrayList<Chord> chords = new ArrayList<Chord>();
 	private static List<String> keys = Arrays.asList("C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B");
 
 	static {
-		// ----- Akkorde -----
-		// Dur
-		chords.add(new Chord("", new int[] { 0, 4, 7 })); // maj, major
-		chords.add(new Chord(" 1. Umkehrung", new int[] { 4, 7, 12 }));
-		chords.add(new Chord(" 2. Umkehrung", new int[] { 7, 12, 16 }));
+		// ----- Intervals -----
+		intervals.add(new Interval("Perfect unison", 0));
+		intervals.add(new Interval("Minor second", 1));
+		intervals.add(new Interval("Major second", 2));
+		intervals.add(new Interval("Minor third", 3));
+		intervals.add(new Interval("Major third", 4));
+		intervals.add(new Interval("Perfect fourth", 5));
+		intervals.add(new Interval("Tritone", 6));
+		intervals.add(new Interval("Perfect fifth", 7));
+		intervals.add(new Interval("Minor sixth", 8));
+		intervals.add(new Interval("Major sixth", 9));
+		intervals.add(new Interval("Minor seventh", 10));
+		intervals.add(new Interval("Major seventh", 11));
+		intervals.add(new Interval("Perfect octave", 12));
 
-		chords.add(new Chord("2", new int[] { 0, 2, 7 })); // sus2
-		chords.add(new Chord("4", new int[] { 0, 5, 7 })); // sus4, m(sus4)
-		chords.add(new Chord("(b5)", new int[] { 0, 4, 6 })); // -
-		chords.add(new Chord("6", new int[] { 0, 4, 7, 9 })); // add6, maj6, add23
-		chords.add(new Chord("maj7", new int[] { 0, 4, 7, 11 })); // j7, M7, ��
-		chords.add(new Chord("maj7(b5)", new int[] { 0, 4, 6, 11 })); // -
-		chords.add(new Chord("maj7(#5)", new int[] { 0, 4, 8, 11 })); // -
-		chords.add(new Chord("add9", new int[] { 0, 4, 7, 14 })); // add2
-		chords.add(new Chord("add21", new int[] { 0, 4, 7, 17 })); // add4
-		chords.add(new Chord("6/9", new int[] { 0, 4, 7, 9, 14 })); // maj6/9
-		chords.add(new Chord("maj7(b9)", new int[] { 0, 4, 7, 11, 13 })); // -
-		chords.add(new Chord("maj7(#9)", new int[] { 0, 4, 7, 11, 15 })); // -
-		chords.add(new Chord("maj7/#11", new int[] { 0, 4, 7, 11, 18 })); // -
-		chords.add(new Chord("maj7/13", new int[] { 0, 4, 7, 11, 21 })); // -
-		chords.add(new Chord("maj9", new int[] { 0, 4, 7, 11, 14 })); // maj7/9
-		chords.add(new Chord("6/9/#11", new int[] { 0, 3, 7, 9, 14, 18 })); // -
-		chords.add(new Chord("maj7(#9)11", new int[] { 0, 4, 7 })); // -
-		chords.add(new Chord("maj9/13", new int[] { 0, 4, 7, 11, 14, 21 })); // maj7/9/13
-		chords.add(new Chord("maj11", new int[] { 0, 4, 7, 11, 14, 17 })); // maj7/9/11, maj9/11
+		// ----- Accords -----
+		// Major
+		chords.add(new Chord("", new int[] {0,4,7}));					// maj, major {c1,e,g}
+		chords.add(new Chord("2", new int[] {0,2,7}));					// sus2 {c1,d,g}
+		chords.add(new Chord("4", new int[] {0,5,7}));					// sus4, m(sus4) {c1,f,g}
+		chords.add(new Chord("(b5)", new int[] {0,4,6}));				// - {c1,e,g&}
+		chords.add(new Chord("6", new int[] {0,4,7,9}));				// add6, maj6, add23 {c1,e,g,a}
+		chords.add(new Chord("maj7", new int[] {0,4,7,11}));			// j7, M7, Δ {c1,e,g,b}
+		chords.add(new Chord("maj7(b5)", new int[] {0,4,6,11}));		// - {c1,e,g&,b}
+		chords.add(new Chord("maj7(#5)", new int[] {0,4,8,11}));		// - {c1,e,g#,b}
+		chords.add(new Chord("add9", new int[] {0,4,7,14}));			// add2 {c1,e,g,d2}
+		chords.add(new Chord("add21", new int[] {0,4,7,17}));			// add4 {c1,e,g,f2}
+		chords.add(new Chord("6/9", new int[] {0,4,7,9,14}));			// maj6/9 {c1,e,g,a,d2}
+		chords.add(new Chord("maj7(b9)", new int[] {0,4,7,11,13}));		// - {c1,e,g,b,d&2}
+		chords.add(new Chord("maj7(#9)", new int[] {0,4,7,11,15}));		// - {c1,e,g,b,d#2}
+		chords.add(new Chord("maj7/#11", new int[] {0,4,7,11,18}));		// - {c1,e,g,b,f#2}
+		chords.add(new Chord("maj7/13", new int[] {0,4,7,11,21}));		// - {c1,e,g,b,a2}
+		chords.add(new Chord("maj9", new int[] {0,4,7,11,14}));			// maj7/9 {c1,e,g,b,d2}
+		chords.add(new Chord("6/9/#11", new int[] {0,3,7,9,14,18}));	// - {c1,e&,g,a,d2,f#}
+		chords.add(new Chord("maj7(#9)11", new int[] {0,4,7,11,15,17})); // - {c1,e,g,b,d#2,f}
+		chords.add(new Chord("maj9/13", new int[] {0,4,7,11,14,21}));	// maj7/9/13 {c1,e,g,b,d2,a}
+		chords.add(new Chord("maj11", new int[] {0,4,7,11,14,17}));		// maj7/9/11, maj9/11 {c1,e,g,b,d2,f}
 
-		// Moll
-		chords.add(new Chord("m", new int[] { 0, 3, 7 })); // min, minor, moll
-		chords.add(new Chord("m6", new int[] { 0, 3, 7, 9 })); // min6
-		chords.add(new Chord("m7", new int[] { 0, 3, 7, 10 })); // -7, mi7, min7
-		chords.add(new Chord("m(maj7)", new int[] { 0, 3, 7, 11 })); // m(j7)
-		chords.add(new Chord("m(add9)", new int[] { 0, 3, 7, 14 })); // m(add2)
-		chords.add(new Chord("m(add21)", new int[] { 0, 3, 7, 17 })); // m(add4)
+		// Minor
+		chords.add(new Chord("m", new int[] {0,3,7}));					// min, minor, moll {c1,e&,g}
+		chords.add(new Chord("m6", new int[] {0,3,7,9}));				// min6 {c1,e&,g,a}
+		chords.add(new Chord("m7", new int[] {0,3,7,10}));				// -7, mi7, min7 {c1,e&,g,b&}
+		chords.add(new Chord("m(maj7)", new int[] {0,3,7,11}));			// m(j7) {c1,e&,g,b}
+		chords.add(new Chord("m(add9)", new int[] {0,3,7,14}));			// m(add2) {c1,e&,g,d2}
+		chords.add(new Chord("m(add21)", new int[] {0,3,7,17}));		// m(add4) {c1,e&,g,f2}
+		chords.add(new Chord("m6/9", new int[] {0,3,7,9,14}));			// min6/9 {c1,e&,g,a,d2}
+		chords.add(new Chord("m7/11", new int[] {0,3,7,10,17}));		// min7/11 {c1,e&,g,b&,f2}
+		chords.add(new Chord("m7/11(b5)", new int[] {0,3,6,10,17}));	// min7/11(b5) {c1,e&,g&,b&,f2}
+		chords.add(new Chord("m7/b13", new int[] {0,3,7,10,20}));		// min7/b13 {c1,e&,g,b&,a&2}
+		chords.add(new Chord("m7/13", new int[] {0,3,7,10,21}));		// min7/13 {c1,e&,g,b&,a2}
+		chords.add(new Chord("m(maj9)", new int[] {0,3,7,11,14}));		// m(j9) {c1,e&,g,b,d2}
+		chords.add(new Chord("m9", new int[] {0,3,7,10,14}));			// min9 {c1,e&,g,b&,d}
+		chords.add(new Chord("m6/9/11", new int[] {0,3,7,9,14,17}));	// min6/9/11 {c1,e&,g,a,d2,f}
+		chords.add(new Chord("m11", new int[] {0,3,7,10,14,17}));		// min11, m7/9/11, m9/11 {c1,e&,g,b&,d2,f}
+		chords.add(new Chord("m13", new int[] {0,3,7,10,14,17,21}));	// min13 {c1,e&,g,b&,d2,f,a}
+
+		// Dominant-Sept
+		chords.add(new Chord("7", new int[] {0,4,7,10}));				// dom7 {c1,e,g,b&}
+		chords.add(new Chord("7/4", new int[] {0,5,7,10}));				// 7sus4 {c1,f,g,b&}
+		chords.add(new Chord("7(b5)", new int[] {0,4,6,10}));			// Ø {c1,e,g&,b&}
+		chords.add(new Chord("7(#5)", new int[] {0,4,8,10}));			// - {c1,e,g#,b&}
+		chords.add(new Chord("7(b9)", new int[] {0,4,7,10,13}));		// dom(b9) {c1,e,g,b&,d&2}
+		chords.add(new Chord("7(b9)4", new int[] {0,5,7,10,13}));		// - {c1,f,g,b&,d&2}
+		chords.add(new Chord("7(#9)", new int[] {0,4,7,10,15}));		// dom(#9) {c1,e,g,b&,d#2}
+		chords.add(new Chord("7/11", new int[] {0,4,7,10,17}));			// - {c1,e,g,b&,f2}
+		chords.add(new Chord("7/#11", new int[] {0,4,7,10,18}));		// - {c1,e,g,b&,f#2}
+		chords.add(new Chord("7/b13", new int[] {0,4,7,10,20}));		// - {c1,e,g,b&,a&2}
+		chords.add(new Chord("7/13", new int[] {0,4,7,10,21}));			// - {c1,e,g,b&,a2}
+		chords.add(new Chord("9", new int[] {0,4,7,10,14}));			// dom9, 7/9 {c1,e,g,b&,d2}
+		chords.add(new Chord("9/4", new int[] {0,5,7,10,14}));			// 9sus4, 11/4, 11sus4 {c1,f,g,b&,d2}
+		chords.add(new Chord("9(b5)", new int[] {0,4,6,10,14}));		// 7/9(b5), 9(#11) {c1,e,g&,b&,d2}
+		chords.add(new Chord("9(#5)", new int[] {0,4,8,10,14}));		// 7/9(#5) {c1,e,g#,b&,d2}
+		chords.add(new Chord("7(b9)#11", new int[] {0,4,7,10,13,18}));	// - {c1,e,g,b&,d&2,f#}
+		chords.add(new Chord("7(b9)b13", new int[] {0,4,7,10,13,20}));	// - {c1,e,g,b&,d&2,a&}
+		chords.add(new Chord("7(b9)13", new int[] {0,4,7,10,13,21}));	// - {c1,e,g,b&,d&2,a}
+		chords.add(new Chord("7(#9)#11", new int[] {0,4,7,10,15,18}));	// - {c1,e,g,b&,d#2,f#}
+		chords.add(new Chord("7(#9)b13", new int[] {0,4,7,10,15,20}));	// - {c1,e,g,b&,d#2,a&}
+		chords.add(new Chord("9/#11", new int[] {0,4,7,10,14,18}));		// - {c1,e,g,b&,d2,f#}
+		chords.add(new Chord("9/b13", new int[] {0,4,7,10,14,20}));		// - {c1,e,g,b&,d2,a&}
+		chords.add(new Chord("9/13", new int[] {0,4,7,10,14,21}));		// 9/6, 7/9/13 {c1,e,g,b&,d2,a}
+		chords.add(new Chord("13/4", new int[] {0,5,7,10,14,21}));		// 13sus4, 9/13sus4 {c1,f,g,b&,d2,a}
+		chords.add(new Chord("11", new int[] {0,4,7,10,14,17}));		// dom11 {c1,e,g,b&,d2,f}
+		chords.add(new Chord("11(b9)", new int[] {0,4,7,10,13,17}));	// - {c1,e,g,b&,d&2,f}
+		chords.add(new Chord("11(#9)", new int[] {0,4,7,10,15,17}));	// - {c1,e,g,b&,d#2,f}
+		chords.add(new Chord("7(#9)#11", new int[] {0,4,7,10,15,18}));	// - {c1,e,g,b&,d#2,f#}
+		chords.add(new Chord("9/#11/13", new int[] {0,4,7,10,14,18,21})); // - {c1,e,g,b&,d2,f#,a}
+		chords.add(new Chord("13", new int[] {0,4,7,10,14,17,21}));		// dom13 {c1,e,g,b&,d2,f,a}
+		chords.add(new Chord("13(b9)", new int[] {0,4,7,10,13,17,21}));	// - {c1,e,g,b&,d&2,f,a}
+		chords.add(new Chord("13(#9)", new int[] {0,4,7,10,15,17,21}));	// - {c1,e,g,b&,d#2,f,a}
+		
+		// Vermindert
+		chords.add(new Chord("°", new int[] {0,3,6}));					// dim, verm, m(b5) {c1,e&,g&}
+		chords.add(new Chord("°7", new int[] {0,3,6,9}));				// - {c1,e&,g&,b&&}
+		chords.add(new Chord("°7/b13", new int[] {0,3,6,9,14}));		// - {c1,e&,g&,b&&,d2}
+		
+		// Halb-Vermindert
+		chords.add(new Chord("m7(b5)", new int[] {0,3,6,10}));			// mØ, dim7, min7(b5) {c1,e&,g&,b&}
+		
+		// Übermäßig
+		chords.add(new Chord("+", new int[] {0,4,8}));					// (#5), aug, überm {c1,e,g#}
+		
+		// Powerchords
+		chords.add(new Chord("1-b3-x", new int[] {0,3}));				// - {c1,e&}
+		chords.add(new Chord("1-3-x", new int[] {0,4}));				// - {c1,e}
+		chords.add(new Chord("1-4-x", new int[] {0,5}));				// - {c1,f}
+		chords.add(new Chord("1-x-b5", new int[] {0,6}));				// - {c1,g&}
+		chords.add(new Chord("5", new int[] {0,7}));					// 1-x-5 {c1,g}
+		chords.add(new Chord("1-x-6", new int[] {0,9}));				// - {c1,a}
+		chords.add(new Chord("1-x-b7", new int[] {0,10}));				// - {c1,b&}
+		chords.add(new Chord("1-x-7", new int[] {0,11}));				// - {c1,b}
+
+	}
+
+	public static String getIntervalName(int[] quintNotes) {
+		int semitones = quintNotes[1] - quintNotes[0];
+		for (int i = 0; i < intervals.size(); i++) {
+			Interval interval = intervals.get(i);
+			if (interval.semitones == semitones) {
+				return interval.name;
+			}
+		}
+		return null;
 	}
 
 	public static String getChordName(int[] notes) {
 		int steps[] = new int[notes.length - 1];
 		for (int i = 1; i < notes.length; i++) {
 			steps[i - 1] = notes[i] - notes[i - 1];
-			System.out.println(notes[i] - notes[i - 1]);
 		}
 
 		Chord chord = getChord(steps);
--- core/src/de/meisterschueler/chords/ChordfinderTest.java
+++ core/src/de/meisterschueler/chords/ChordfinderTest.java
@@ -1,5 +1,3 @@
-package de.meisterschueler.chords;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -8,23 +6,27 @@ public class ChordfinderTest {
 
 	@Test
 	public void chordTest() {
-		int cMajorNotes[] = {48, 52, 55};
+		int cMajorNotes[] = { 48, 52, 55 };
 		String cMajorName = Chordfinder.getChordName(cMajorNotes);
 		assertEquals("C", cMajorName);
-		
-		int fSharpMinorNotes[] = {54, 57, 61};
+
+		int fSharpMinorNotes[] = { 54, 57, 61 };
 		String fSharpMinorName = Chordfinder.getChordName(fSharpMinorNotes);
 		assertEquals("F#m", fSharpMinorName);
-		
-		int lowestChordNotes[] = {0, 4, 7};
+
+		int lowestChordNotes[] = { 0, 4, 7 };
 		String lowestChordName = Chordfinder.getChordName(lowestChordNotes);
-		
-		int highestChordNotes[] = {120, 124, 127};
+		assertEquals("C", lowestChordName);
+
+		int highestChordNotes[] = { 120, 124, 127 };
 		String highestChordName = Chordfinder.getChordName(highestChordNotes);
+		assertEquals("C", highestChordName);
 	}
-	
+
 	@Test
 	public void intervalTest() {
-		fail("not implemented");
+		int quintNotes[] = {0, 7};
+		String quintName = Chordfinder.getIntervalName(quintNotes);
+		assertEquals("Perfect fifth", quintName);
 	}
 }
