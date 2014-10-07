15c351cba19dad71029ff13e53c535cf6fc13bb7
Konstantin Gründger <kammermark@gmx.de>
2014-10-07 23:40:41 +0200

    Refactoring

--- core/src/de/meisterschueler/gdx/screens/SynthesiaScreen.java
+++ core/src/de/meisterschueler/gdx/screens/SynthesiaScreen.java
@@ -23,25 +23,35 @@ public class SynthesiaScreen extends MidiScreen {
 	private static final float black_small_width = 12;
 	private static final float octave_width = 151;
 
+	private enum KeyType {
+		WHITE, BLACK
+	};
+
 	private class Key extends Actor {
 
 		private ShapeRenderer shapeRenderer;
 		private int index;
-		private float xoffset;
 		private boolean pressed;
 
 		private float scale;
+		private KeyType keyType;
+
+		private float x;
+		private float y;
+		private float width;
+		private float height;
 
 		private static final float white_single_width = (1 * white_small_width + 0 * black_small_width) / 1; // {c}
 		private static final float white_double_width = (2 * white_small_width + 1 * black_small_width) / 2; // {a,b}
 		private static final float white_triple_width = (3 * white_small_width + 2 * black_small_width) / 3; // {c,d,e}
 		private static final float white_quadrupel_width = (4 * white_small_width + 3 * black_small_width) / 4; // {f,g,a,b}
+		private static final float white_quintupel_width = (5 * white_small_width + 4 * black_small_width) / 5; // {c,d,e,f,g}
 
 		public Key(final int index) {
 			shapeRenderer = new ShapeRenderer();
 
 			this.index = index;
-			this.xoffset = (index / 12) * (7 * white_small_width + 5 * black_small_width);
+
 			this.scale = octave_width / (7 * white_small_width + 5 * black_small_width);
 
 			this.addListener(new ClickListener() {
@@ -57,151 +67,179 @@ public class SynthesiaScreen extends MidiScreen {
 					}
 				}
 			});
+
+			calcGeometry();
 		}
 
 		@Override
 		public void draw(Batch batch, float parentAlpha) {
 			batch.end();
 
-			shapeRenderer.begin(ShapeType.Filled);
-			float keyOffset;
-			if (index >= 21 && index <= 23) {
-				switch (index) {
-				case 21:
-					keyOffset = xoffset + (5 * white_small_width + 4 * black_small_width);
-					drawWhiteKey(keyOffset, white_double_width);
-					break;
-				case 22:
-					keyOffset = calcBlackKeyOffset(6, 4);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 23:
-					keyOffset = xoffset + (5 * white_small_width + 4 * black_small_width + white_double_width);
-					drawWhiteKey(keyOffset, white_double_width);
-					break;
-				}
-			} else if (index == 108) {
-				keyOffset = calcWhiteKeyOffset(0, 0);
-				drawWhiteKey(keyOffset, white_single_width);
+			Color colorNormal;
+			Color colorPressed;
+
+			if (keyType == KeyType.WHITE) {
+				colorNormal = Color.WHITE;
+				colorPressed = Color.RED;
 			} else {
-				switch (index % 12) {
-				case 0:
-					keyOffset = calcWhiteKeyOffset(0, 0);
-					drawWhiteKey(keyOffset, white_triple_width);
-					break;
-				case 1:
-					keyOffset = calcBlackKeyOffset(1, 0);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 2:
-					keyOffset = calcWhiteKeyOffset(1, 0);
-					drawWhiteKey(keyOffset, white_triple_width);
-					break;
-				case 3:
-					keyOffset = calcBlackKeyOffset(2, 1);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 4:
-					keyOffset = calcWhiteKeyOffset(2, 0);
-					drawWhiteKey(keyOffset, white_triple_width);
-					break;
-				case 5:
-					keyOffset = calcWhiteKeyOffset(3, 0);
-					drawWhiteKey(keyOffset, white_quadrupel_width);
-					break;
-				case 6:
-					keyOffset = calcBlackKeyOffset(4, 2);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 7:
-					keyOffset = calcWhiteKeyOffset(3, 1);
-					drawWhiteKey(keyOffset, white_quadrupel_width);
-					break;
-				case 8:
-					keyOffset = calcBlackKeyOffset(5, 3);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 9:
-					keyOffset = calcWhiteKeyOffset(3, 2);
-					drawWhiteKey(keyOffset, white_quadrupel_width);
-					break;
-				case 10:
-					keyOffset = calcBlackKeyOffset(6, 4);
-					drawBlackKey(keyOffset, black_small_width);
-					break;
-				case 11:
-					keyOffset = calcWhiteKeyOffset(3, 3);
-					drawWhiteKey(keyOffset, white_quadrupel_width);
-					break;
-				}
+				colorNormal = Color.BLACK;
+				colorPressed = Color.RED;
+			}
+
+			shapeRenderer.begin(ShapeType.Filled);
+			shapeRenderer.setColor(Color.BLACK);
+			shapeRenderer.rect(x, y, width, height);
+			if (pressed) {
+				shapeRenderer.rect(x + 1, y + 1, (width - 2) * scale, height - 1, colorPressed, colorPressed, colorNormal, colorNormal);
+			} else {
+				shapeRenderer.setColor(colorNormal);
+				shapeRenderer.rect(x + 1, y + 1, (width - 2) * scale, height - 1);
 			}
 			shapeRenderer.end();
 
 			batch.begin();
 		}
 
-		private float calcWhiteKeyOffset(int n_triple_width, int n_quadrupel_width) {
-			return n_triple_width * white_triple_width + n_quadrupel_width * white_quadrupel_width + xoffset;
+		private void calcGeometry() {
+			float keyOffset;
+			if (index >= 21 && index <= 23) {
+				switch (index) {
+				case 21:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 0, 0, 5);
+					calcKeyGeometry(keyOffset, white_double_width);
+					break;
+				case 22:
+					keyType = KeyType.BLACK;
+					float xoffset = (index / 12) * (7 * white_small_width + 5 * black_small_width);
+					keyOffset = calcBlackKeyOffset(6, 4);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 23:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(1, 0, 0, 5);
+					calcKeyGeometry(keyOffset, white_double_width);
+					break;
+				}
+			} else if (index == 108) {
+				keyOffset = calcWhiteKeyOffset(0, 0, 0, 0);
+				calcKeyGeometry(keyOffset, white_single_width);
+			} else {
+				switch (index % 12) {
+				case 0:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 0, 0, 0);
+					calcKeyGeometry(keyOffset, white_triple_width);
+					break;
+				case 1:
+					keyType = KeyType.BLACK;
+					keyOffset = calcBlackKeyOffset(1, 0);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 2:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 1, 0, 0);
+					calcKeyGeometry(keyOffset, white_triple_width);
+					break;
+				case 3:
+					keyType = KeyType.BLACK;
+					keyOffset = calcBlackKeyOffset(2, 1);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 4:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 2, 0, 0);
+					calcKeyGeometry(keyOffset, white_triple_width);
+					break;
+				case 5:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 3, 0, 0);
+					calcKeyGeometry(keyOffset, white_quadrupel_width);
+					break;
+				case 6:
+					keyType = KeyType.BLACK;
+					keyOffset = calcBlackKeyOffset(4, 2);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 7:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 3, 1, 0);
+					calcKeyGeometry(keyOffset, white_quadrupel_width);
+					break;
+				case 8:
+					keyType = KeyType.BLACK;
+					keyOffset = calcBlackKeyOffset(5, 3);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 9:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 3, 2, 0);
+					calcKeyGeometry(keyOffset, white_quadrupel_width);
+					break;
+				case 10:
+					keyType = KeyType.BLACK;
+					keyOffset = calcBlackKeyOffset(6, 4);
+					calcKeyGeometry(keyOffset, black_small_width);
+					break;
+				case 11:
+					keyType = KeyType.WHITE;
+					keyOffset = calcWhiteKeyOffset(0, 3, 3, 0);
+					calcKeyGeometry(keyOffset, white_quadrupel_width);
+					break;
+				}
+			}
+		}
+
+		private float calcWhiteKeyOffset(int n_double_width, int n_triple_width, int n_quadrupel_width, int n_quintupel_width) {
+			float octaveOffset = (index / 12) * (7 * white_small_width + 5 * black_small_width);
+			return n_double_width * white_double_width + n_triple_width * white_triple_width + n_quadrupel_width * white_quadrupel_width + n_quintupel_width
+					* white_quintupel_width + octaveOffset;
 		}
 
 		private float calcBlackKeyOffset(int n_white_small_width, int n_black_small_width) {
-			return n_white_small_width * white_small_width + n_black_small_width * black_small_width + xoffset;
+			float octaveOffset = (index / 12) * (7 * white_small_width + 5 * black_small_width);
+			return n_white_small_width * white_small_width + n_black_small_width * black_small_width + octaveOffset;
 		}
 
-		private void drawWhiteKey(float keyOffset, float width) {
-			float height = 150;
-			float yoffset = 0;
+		private void calcKeyGeometry(float keyOffset, float keyWidth) {
+			x = keyOffset * scale;
+			width = keyWidth * scale;
 
-			shapeRenderer.setColor(Color.BLACK);
-			shapeRenderer.rect(keyOffset * scale, yoffset, width * scale, height);
-			if (pressed) {
-				shapeRenderer.rect((keyOffset + 1) * scale, yoffset + 1, (width - 2) * scale, height - 1, Color.RED, Color.RED, Color.WHITE, Color.WHITE);
+			if (keyType == KeyType.WHITE) {
+				y = 0;
+				height = 150;
 			} else {
-				shapeRenderer.setColor(Color.WHITE);
-				shapeRenderer.rect((keyOffset + 1) * scale, yoffset + 1, (width - 2) * scale, height - 1);
+				y = 50;
+				height = 100;
 			}
 
 			setWidth(width);
 			setHeight(height);
-			setBounds(keyOffset * scale, 0, width, height);
+			setBounds(x, y, width, height);
 		}
 
-		private void drawBlackKey(float keyOffset, float width) {
-			float height = 100;
-			float yoffset = 50;
-
-			shapeRenderer.setColor(Color.BLACK);
-			shapeRenderer.rect(keyOffset * scale, yoffset, width * scale, height);
-			if (pressed) {
-				shapeRenderer.rect((keyOffset + 1) * scale, yoffset + 1, (width - 2) * scale, height - 1, Color.RED, Color.RED, Color.BLACK, Color.BLACK);
-			} else {
-				shapeRenderer.setColor(Color.BLACK);
-				shapeRenderer.rect((keyOffset + 1) * scale, yoffset + 1, (width - 2) * scale, height - 1);
-			}
-
-			setWidth(width);
-			setHeight(height);
-			setBounds(keyOffset * scale, 50, width, height);
+		public KeyType getKeyType() {
+			return keyType;
 		}
 	}
 
 	private class Bubble extends Actor {
-	
+
 		private ShapeRenderer shapeRenderer;
-	
+
 		public Bubble() {
 			this.shapeRenderer = new ShapeRenderer();
 		}
-	
+
 		@Override
 		public void draw(Batch batch, float parentAlpha) {
 			batch.end();
-			
+
 			shapeRenderer.begin(ShapeType.Filled);
 			shapeRenderer.setColor(Color.RED);
 			shapeRenderer.circle(200, 200, 10);
 			shapeRenderer.end();
-	
+
 			batch.begin();
 		}
 	}
@@ -212,7 +250,7 @@ public class SynthesiaScreen extends MidiScreen {
 			Bubble bubble = new Bubble();
 			bubble.addAction(Actions.moveTo(0, 0, 10));
 			addActor(bubble);
-			
+
 			Bubble bubble2 = new Bubble();
 			bubble2.addAction(Actions.moveTo(50, 50, 5));
 			addActor(bubble2);
@@ -264,24 +302,14 @@ public class SynthesiaScreen extends MidiScreen {
 		Group whiteKeys = new Group();
 		Group blackKeys = new Group();
 
-		whiteKeys.addActor(new Key(21));
-		blackKeys.addActor(new Key(22));
-		whiteKeys.addActor(new Key(23));
-		for (int octave = 2; octave < 9; octave++) {
-			whiteKeys.addActor(new Key(0 + 12 * octave));
-			blackKeys.addActor(new Key(1 + 12 * octave));
-			whiteKeys.addActor(new Key(2 + 12 * octave));
-			blackKeys.addActor(new Key(3 + 12 * octave));
-			whiteKeys.addActor(new Key(4 + 12 * octave));
-			whiteKeys.addActor(new Key(5 + 12 * octave));
-			blackKeys.addActor(new Key(6 + 12 * octave));
-			whiteKeys.addActor(new Key(7 + 12 * octave));
-			blackKeys.addActor(new Key(8 + 12 * octave));
-			whiteKeys.addActor(new Key(9 + 12 * octave));
-			blackKeys.addActor(new Key(10 + 12 * octave));
-			whiteKeys.addActor(new Key(11 + 12 * octave));
+		for (int i = 21; i <= 108; i++) {
+			Key key = new Key(i);
+			if (key.getKeyType() == KeyType.WHITE) {
+				whiteKeys.addActor(key);
+			} else {
+				blackKeys.addActor(key);
+			}
 		}
-		whiteKeys.addActor(new Key(108));
 
 		Group keyboard = new Group();
 		keyboard.addActor(whiteKeys);
