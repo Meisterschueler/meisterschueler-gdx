7d8d121d29e95f0368d2baa922478c4d2cac6a72
kammermark@gmx.de <kammermark@gmx.de>
2014-10-09 16:19:01 +0200

    Removed useless import

--- core/src/de/meisterschueler/gdx/screens/ScrollEffect.java
+++ core/src/de/meisterschueler/gdx/screens/ScrollEffect.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
