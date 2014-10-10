99008901062b3a87f40726f15d5cdce2bc394ee9
kammermark@gmx.de <kammermark@gmx.de>
2014-10-10 16:56:51 +0200

    Remove unused import

--- core/src/de/meisterschueler/gdx/screens/legato/LegatoScreen.java
+++ core/src/de/meisterschueler/gdx/screens/legato/LegatoScreen.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
