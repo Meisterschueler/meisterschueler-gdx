2a9b39ffbeb38a4e4d079ce5b3300f96922dcac8
kammermark@gmx.de <kammermark@gmx.de>
2014-10-21 18:55:18 +0200

    Add Event and Score functionality

--- core/src/de/meisterschueler/gdx/screens/GpgsScreen.java
+++ core/src/de/meisterschueler/gdx/screens/GpgsScreen.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldFilter;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import de.meisterschueler.gdx.Meisterschueler;
@@ -17,6 +19,9 @@ public class GpgsScreen extends MidiScreen {
 	private Table table;
 	private Label labelLogin;
 	private TextButton buttonLogin;
+	private TextButton buttonEvent;
+	private TextField fieldScore;
+	private TextButton buttonScore;
 	private TextButton buttonLeaderboard;
 	
 	public GpgsScreen() {
@@ -32,6 +37,39 @@ public class GpgsScreen extends MidiScreen {
 			}
 		});
 		
+		buttonEvent = new TextButton("Event", skin);
+		buttonEvent.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				scoreService.submitEventGPGS_chromaticStart();
+			}
+		});
+		
+		fieldScore = new TextField("0", skin);
+		fieldScore.setTextFieldFilter(new TextFieldFilter() {
+
+			@Override
+			public boolean acceptChar(TextField textField, char c) {
+				if (c >= 48 && c <= 57) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+		
+		buttonScore = new TextButton("Score", skin);
+		buttonScore.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				String scores = fieldScore.getText();
+				if (scores.isEmpty()) {
+					scores = "0";
+				}
+				scoreService.submitScoreGPGS_chromatic(Integer.valueOf(scores));
+			}
+		});
+		
 		buttonLeaderboard = new TextButton("Leaderboard", skin);
 		buttonLeaderboard.addListener(new ClickListener() {
 			@Override
@@ -42,11 +80,15 @@ public class GpgsScreen extends MidiScreen {
 		
 		table = new Table();
 		table.setFillParent(true);
-		table.add(labelLogin).prefSize(200, 60);
-		table.row();
+		table.add(labelLogin).prefSize(60, 60);
 		table.add(buttonLogin).prefSize(200, 60);
 		table.row();
-		table.add(buttonLeaderboard).prefSize(200, 60);
+		table.add(buttonEvent).colspan(2).prefSize(260, 60);
+		table.row();
+		table.add(fieldScore).prefSize(60, 60);
+		table.add(buttonScore).prefSize(200, 60);
+		table.row();
+		table.add(buttonLeaderboard).colspan(2).prefSize(260, 60);
 		
 		uiGroup.addActor(table);
 	}
@@ -54,10 +96,10 @@ public class GpgsScreen extends MidiScreen {
 	@Override
 	public void render(float delta) {
 		if (scoreService.getSignedInGPGS()) {
-			labelLogin.setText("Logged in");
+			labelLogin.setText("OK");
 			buttonLogin.setText("Logout");
 		} else {
-			labelLogin.setText("Not logged in");
+			labelLogin.setText("!OK");
 			buttonLogin.setText("Login");
 		}
 		super.render(delta);
